508adf0861e6eee7634e0f651c21903492136e36
jice1001 <784599938@qq.com>
2016-11-12 16:26:37 +0800

    修改gate_wrap

--- plugin/gate/Gate_Wrap.h
+++ plugin/gate/Gate_Wrap.h
@@ -14,9 +14,9 @@ using namespace v8;
 
 //函数说明：初始化gate		参数：无 返回值：无
 void init_gate(const FunctionCallbackInfo<Value>& args);
-//函数说明：增加客户端连接  	参数：1.session    返回值：无
+//函数说明：增加客户端连接  	参数：1.session信息    返回值：无
 void add_session(const FunctionCallbackInfo<Value>& args);
-//函数说明：移除客户端连接  	参数：1.eid 2.drop_cid 3.error_code    返回值：无
+//函数说明：移除客户端连接  	参数：1.cid    返回值：无
 void remove_session(const FunctionCallbackInfo<Value>& args);
 
 
